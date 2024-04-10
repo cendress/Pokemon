c6ed767fcd529b2f945377833d3bd179438958ef
cendress <95492688+cendress@users.noreply.github.com>
2024-04-10 15:28:26 -0400

    Configure title label

--- Pokemon/SearchVC.swift
+++ Pokemon/SearchVC.swift
@@ -11,7 +11,25 @@ class SearchVC: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    view.backgroundColor = .red
+    view.backgroundColor = .systemBackground
+    
+    setupViews()
+  }
+  
+  private func setupViews() {
+    let title = UILabel()
+    title.text = "Pokémon Search"
+    title.font = UIFont(name: "Avenir", size: 40)
+    title.textColor = .label
+    
+    view.addSubview(title)
+    
+    title.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      title.topAnchor.constraint(equalTo: view.topAnchor, constant: 300),
+      title.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+    ])
   }
 
 
