715ec652ae808a318011f7836ba82c0e669c1ccb
cendress <95492688+cendress@users.noreply.github.com>
2024-04-11 14:07:54 -0400

    Implement go button

--- Pokemon/SearchVC.swift
+++ Pokemon/SearchVC.swift
@@ -31,11 +31,25 @@ class SearchVC: UIViewController {
     searchBar.clearButtonMode = .whileEditing
     searchBar.borderStyle = .none
     
+    let goButton = UIButton()
+    goButton.setTitle("Go", for: .normal)
+    goButton.setTitleColor(.white, for: .normal)
+    goButton.backgroundColor = UIColor.green
+    goButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
+    goButton.layer.cornerRadius = 10
+    goButton.clipsToBounds = true
+    goButton.layer.shadowColor = UIColor.black.cgColor
+    goButton.layer.shadowOffset = CGSize(width: 0, height: 2)
+    goButton.layer.shadowRadius = 6
+    goButton.layer.shadowOpacity = 0.3
+    
     view.addSubview(title)
     view.addSubview(searchBar)
+    view.addSubview(goButton)
     
     title.translatesAutoresizingMaskIntoConstraints = false
     searchBar.translatesAutoresizingMaskIntoConstraints = false
+    goButton.translatesAutoresizingMaskIntoConstraints = false
     
     NSLayoutConstraint.activate([
       title.centerXAnchor.constraint(equalTo: view.centerXAnchor),
@@ -45,10 +59,12 @@ class SearchVC: UIViewController {
       searchBar.centerYAnchor.constraint(equalTo: view.centerYAnchor),
       searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
       searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
-      searchBar.heightAnchor.constraint(equalToConstant: 60)
+      searchBar.heightAnchor.constraint(equalToConstant: 60),
+      
+      goButton.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 60),
+      goButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      goButton.heightAnchor.constraint(equalToConstant: 100),
+      goButton.widthAnchor.constraint(equalToConstant: 200)
     ])
   }
-
-
 }
-
