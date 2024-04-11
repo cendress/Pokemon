a323fead62d3db71f445ee715c15fa810364e4a3
cendress <95492688+cendress@users.noreply.github.com>
2024-04-11 15:26:33 -0400

    Create Pokemon data model

---
+++ Pokemon/Pokemon.swift
@@ -0,0 +1,15 @@
+//
+//  Pokemon.swift
+//  Pokemon
+//
+//  Created by Christopher Endress on 4/11/24.
+//
+
+import Foundation
+
+struct Pokemon: Codable {
+  let id: Int
+  let name: String
+  let sprite: String
+  let type: String
+}
